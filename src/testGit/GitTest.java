150ed38a0bf08d7513885d4c4c0af5041b9d6c7f
Deena Rubin <dgrubin1@neiu.edu>
2014-02-07 16:04:30 -0600

    version1

--- src/testGit/GitTest.java
+++ src/testGit/GitTest.java
@@ -3,6 +3,7 @@ package testGit;
 public class GitTest {
 	public static void main (String[] args){
 		System.out.println("Deena's Git Test");
+		System.out.println("New changes for version1");
 	}
 
 }
